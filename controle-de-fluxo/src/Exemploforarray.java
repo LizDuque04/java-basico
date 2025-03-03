10dbd7413146eed9e455bfcbf67fb13b90c7800f
Liz <elizabete.duque2002@gmail.com>
2025-03-03 14:17:38 -0300

    for em arrays

---
+++ controle-de-fluxo/src/Exemploforarray.java
@@ -0,0 +1,9 @@
+public class Exemploforarray {
+    public static void main(String[] args) {
+        String comidas [] = {"Pizza", "Lasanha", "Uva", "Batata", "Arroz"};
+
+        for(int x = 0; x < comidas.length; x++){
+            System.out.println("A comida no indice X: " + x + " é " + comidas[x]);
+        }
+    }
+}
