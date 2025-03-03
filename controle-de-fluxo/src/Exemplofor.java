207cad69ebda8ff5720804116b18cf079ca3a601
Liz <elizabete.duque2002@gmail.com>
2025-03-03 14:06:30 -0300

    estudo de repetição for

---
+++ controle-de-fluxo/src/Exemplofor.java
@@ -0,0 +1,7 @@
+public class Exemplofor {
+    public static void main(String[] args) {
+        for(int carneirinho = 1; carneirinho <= 20; carneirinho++){
+            System.out.println(carneirinho + " carneirinho");
+        }
+    }
+}
