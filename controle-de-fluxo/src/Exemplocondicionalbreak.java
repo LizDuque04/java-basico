dd95b731feeead2cd2db9a0a439f13387b639816
Liz <elizabete.duque2002@gmail.com>
2025-03-03 14:21:28 -0300

    for com break

---
+++ controle-de-fluxo/src/Exemplocondicionalbreak.java
@@ -0,0 +1,11 @@
+public class Exemplocondicionalbreak {
+    public static void main(String[] args) {
+        for(int numero = 1; numero <= 5; numero++){
+            if(numero == 3)
+                break;
+
+            System.out.println(numero);
+        }
+    }
+}
+//break para a aplicação e continue pula a interação que foi solicitada.
