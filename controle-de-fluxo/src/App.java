f44d76808b3137526a759bd176d63fbcf109336c
Liz <elizabete.duque2002@gmail.com>
2025-03-03 12:36:33 -0300

    Terminal

--- controle-de-fluxo/src/App.java
+++ controle-de-fluxo/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esse programa foi executado no terminal.");
+        System.out.println("Hello, World!");
     }
 }
