7d6c99f92a56623f7097154857feada2c99e6bde
Liz <elizabete.duque2002@gmail.com>
2025-03-03 09:15:57 -0300

    Modificado

--- MinhaClasse/src/App.java
+++ MinhaClasse/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception { int numero1 = 1; String numero2 = "2"; System.out.println(numero1+numero2); 
-}
+    public static void main(String[] args) throws Exception {
+        System.out.println("Esse programa foi executado pelo terminal!");
     }
-
+}
